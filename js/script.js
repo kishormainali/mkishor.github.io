4942f2f915f59038c11b8182fcb10bdc523b6767
Kishor Mainali <33093136+kishor98100@users.noreply.github.com>
2020-11-12 10:03:04 +0545

    initial

---
+++ js/script.js
@@ -0,0 +1,146 @@
+window.addEventListener("load", function () {
+  document.querySelector(".preloader").classList.add("opacity-0");
+  this.setTimeout(function () {
+    document.querySelector(".preloader").style.display = "none";
+  }, 1000);
+});
+
+// portfolio item filter
+
+const filterContainer = document.querySelector(".portfolio-filter"),
+  filterBtns = filterContainer.children,
+  totalFilterBtn = filterBtns.length,
+  portfolioItems = document.querySelectorAll(".portfolio-item"),
+  totalPortfolioItem = portfolioItems.length;
+
+for (let i = 0; i < totalFilterBtn; i++) {
+  filterBtns[i].addEventListener("click", function () {
+    filterContainer.querySelector(".active").classList.remove("active");
+    this.classList.add("active");
+
+    const filterValue = this.getAttribute("data-filter");
+
+    for (let k = 0; k < totalPortfolioItem; k++) {
+      if (filterValue === portfolioItems[k].getAttribute("data-category")) {
+        portfolioItems[k].classList.remove("hide");
+        portfolioItems[k].classList.add("show");
+      } else {
+        portfolioItems[k].classList.remove("show");
+        portfolioItems[k].classList.add("hide");
+      }
+      if (filterValue === "all") {
+        portfolioItems[k].classList.remove("hide");
+        portfolioItems[k].classList.add("show");
+      }
+    }
+  });
+}
+
+// portfolio lightbox
+
+const lightbox = document.querySelector(".lightbox"),
+  lightboxImg = lightbox.querySelector(".lightbox-img"),
+  closeLightbox = lightbox.querySelector(".lightbox-close i"),
+  lightboxText = lightbox.querySelector(".caption-text"),
+  lightboxCounter = lightbox.querySelector(".caption-counter");
+
+let itemIndex = 0;
+
+for (let i = 0; i < totalPortfolioItem; i++) {
+  portfolioItems[i].addEventListener("click", function () {
+    itemIndex = i;
+    changeItem();
+    toggleLightBox();
+  });
+}
+
+function prevItem() {
+  if (itemIndex === 0) {
+    itemIndex = totalPortfolioItem - 1;
+  } else {
+    itemIndex--;
+  }
+  changeItem();
+}
+function nextItem() {
+  if (itemIndex === totalPortfolioItem - 1) {
+    itemIndex = 0;
+  } else {
+    itemIndex++;
+  }
+  changeItem();
+}
+
+function toggleLightBox() {
+  lightbox.classList.toggle("open");
+}
+
+function changeItem() {
+  imgSrc = portfolioItems[itemIndex]
+    .querySelector(".portfolio-img img")
+    .getAttribute("src");
+  lightboxImg.src = imgSrc;
+  lightboxText.innerHTML = portfolioItems[itemIndex].querySelector(
+    "h4"
+  ).innerHTML;
+  lightboxCounter.innerHTML = itemIndex + " of " + totalPortfolioItem;
+}
+
+// close lightbox
+
+lightbox.addEventListener("click", function (event) {
+  if (event.target === closeLightbox || event.target === lightbox) {
+    toggleLightBox();
+  }
+});
+
+// Aside Navbar
+
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
+
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerButton();
+    }
+  });
+}
+
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+
+const navToggler = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navToggler.addEventListener("click", function () {
+  asideSectionTogglerButton();
+});
+
+function asideSectionTogglerButton() {
+  aside.classList.toggle("open");
+  navToggler.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
